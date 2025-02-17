f3787cbb7b5b0196d588821b2fb2fa51d0c63d8c
Dmitrii <140065345+Snowy0Panther@users.noreply.github.com>
2025-02-17 18:22:42 +0300

    MatchGame is finished!

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MatchGame
 {
@@ -20,11 +21,29 @@ namespace MatchGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int mathcesFound;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+
             SetUpGame(); // we setup our game
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed/10F).ToString("0.0s");
+            if(mathcesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+            }
         }
 
         private void SetUpGame() // our function for setup game
@@ -48,16 +67,57 @@ namespace MatchGame
             // repeat commands for each elemnts
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) 
             {
-                // chose random number between 0 to last count emoji elemnt in list
-                // and set name for number index
-                int index = random.Next(animalEmoji.Count);
+                if(textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    // chose random number between 0 to last count emoji elemnt in list
+                    // and set name for number index
+                    int index = random.Next(animalEmoji.Count);
 
-                // Use random number with name index for the getting randrom emoji from list
-                string nextEmoji = animalEmoji[index];
-                // Update TextBlock with randrom emoji from list
-                textBlock.Text = nextEmoji;
-                // Remove random emoji from list
-                animalEmoji.RemoveAt(index);
+                    // Use random number with name index for the getting randrom emoji from list
+                    string nextEmoji = animalEmoji[index];
+                    // Update TextBlock with randrom emoji from list
+                    textBlock.Text = nextEmoji;
+                    // Remove random emoji from list
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            mathcesFound = 0;
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock; 
+            if(findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                mathcesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(mathcesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
